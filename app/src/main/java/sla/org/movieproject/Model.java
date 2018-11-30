6934c93b94e76fec7e08dfb7bbd5b9365653444e
andrewrodebaugh <aquahydration@gmail.com>
2018-11-30 13:12:42 -0500

    New Model

---
+++ app/src/main/java/sla/org/movieproject/Model.java
@@ -0,0 +1,11 @@
+package sla.org.movieproject;
+
+public interface Model {
+    void previous();
+    void next();
+    String title();
+    String subTitle();
+    String description();
+    String image();
+
+}
